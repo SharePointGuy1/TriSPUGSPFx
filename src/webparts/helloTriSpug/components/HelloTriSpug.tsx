837d160e2ae1f1dd4706469a6318d72fa4b6caee
Robert Holmes <rholmes@sharepointguy.com>
2017-07-19 19:09:38 -0400

    Modified

--- src/webparts/helloTriSpug/components/HelloTriSpug.tsx
+++ src/webparts/helloTriSpug/components/HelloTriSpug.tsx
@@ -10,7 +10,7 @@ export default class HelloTriSpug extends React.Component<IHelloTriSpugProps, vo
         <div className={styles.container}>
           <div className={`ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`}>
             <div className="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-              <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+              <span className="ms-font-xl ms-fontColor-white">Welcome to SharePoint! (Azure)</span>
               <p className="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
               <p className="ms-font-l ms-fontColor-white">{escape(this.props.description)}</p>
               <p className="ms-font-l ms-fontColor-white">Purpose: {escape(this.props.purpose)}</p>
